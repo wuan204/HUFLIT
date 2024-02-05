c7a6aea314527e5b6e13c6cf12a62fae60195814
wuan204 <hokhanhdan1@gmail.com>
2024-02-05 16:58:41 +0700

    hoan thanh quan-ok

--- app/src/main/java/com/example/huflit/Language_Setting.java
+++ app/src/main/java/com/example/huflit/Language_Setting.java
@@ -1,14 +1,37 @@
 package com.example.huflit;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class Language_Setting extends AppCompatActivity {
+
+    ImageView imgBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_setting);
+
+        imgBack = findViewById(R.id.imgBack);
+
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
+
+
