1d58e1c5563972414b8d998597e056d11083333c
Mtiennnn <nmt1532003@gmail.com>
2024-02-17 16:00:10 +0700

    Updated files

--- app/src/main/java/com/example/huflit/Seach.java
+++ app/src/main/java/com/example/huflit/Seach.java
@@ -1,6 +1,7 @@
 package com.example.huflit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar; // Import the correct Toolbar class
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class Seach extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seach);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Check if getSupportActionBar() is not null before calling setDisplayHomeAsUpEnabled
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
