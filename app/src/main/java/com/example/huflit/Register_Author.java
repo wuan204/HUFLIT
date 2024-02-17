5505d9696eb0db21a009b74650a3805d464706b4
Mtiennnn <nmt1532003@gmail.com>
2024-02-17 16:06:46 +0700

    Add submodule HUFLIT

--- app/src/main/java/com/example/huflit/Register_Author.java
+++ app/src/main/java/com/example/huflit/Register_Author.java
@@ -3,12 +3,20 @@ package com.example.huflit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class Register_Author extends AppCompatActivity {
+
+    private Toolbar supportActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_author);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
