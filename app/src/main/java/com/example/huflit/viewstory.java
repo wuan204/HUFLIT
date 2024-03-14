4d94357f27536c9587554a522966c3baac631536
wuan204 <144576545+wuan204@users.noreply.github.com>
2024-03-14 14:03:46 +0700

    ok

--- app/src/main/java/com/example/huflit/viewstory.java
+++ app/src/main/java/com/example/huflit/viewstory.java
@@ -2,24 +2,15 @@ package com.example.huflit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.renderscript.Sampler;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.android.volley.Request;
+
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.example.huflit.object.Truyen;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.jar.JarException;
 
 public class viewstory extends AppCompatActivity {
     private ImageView mbtBack, mbtRp, mbtLove, mbtFl, mbtDown, mbtCmt,mimgStory;
