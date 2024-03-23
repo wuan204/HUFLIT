fd2d8e33b8bfde0aad39a9df480349b2c2b5e4ba
Mtiennnn <nmt1532003@gmail.com>
2024-03-23 21:44:15 +0700

    Xong r nek

--- app/src/main/java/com/example/huflit/QLyDanhMuc.java
+++ app/src/main/java/com/example/huflit/QLyDanhMuc.java
@@ -1,10 +1,15 @@
     package com.example.huflit;
 
     import android.annotation.SuppressLint;
+    import android.app.Dialog;
     import android.content.Intent;
     import android.os.Bundle;
+    import android.util.Log;
+    import android.view.Gravity;
     import android.view.View;
+    import android.view.WindowManager;
     import android.widget.AdapterView;
+    import android.widget.Button;
     import android.widget.EditText;
     import android.widget.ImageView;
     import android.widget.ListView;
@@ -22,9 +27,12 @@
     import com.android.volley.toolbox.StringRequest;
     import com.android.volley.toolbox.Volley;
     import com.example.huflit.adapter.DanhMucQLyAdapter;
+    import com.example.huflit.adapter.IOnclickItem;
     import com.example.huflit.api.ApiDanhMucQLy;
     import com.example.huflit.interfaces.LayDanhMucVe;
     import com.example.huflit.object.DanhMucQly;
+    import com.google.android.gms.tasks.OnFailureListener;
+    import com.google.android.gms.tasks.OnSuccessListener;
 
     import org.json.JSONArray;
     import org.json.JSONException;
@@ -36,7 +44,7 @@
 
     import android.view.Menu;
     import android.view.MenuItem;
-    public class QLyDanhMuc extends AppCompatActivity implements LayDanhMucVe {
+    public class QLyDanhMuc extends AppCompatActivity implements LayDanhMucVe, IOnclickItem {
         DanhMucQly danhMucQly;
         ListView lsvDanhMucQly;
         EditText medtAddname;
@@ -75,7 +83,7 @@
         private void  init(){
             arrDanhMuc = new ArrayList<>();
 
-            danhMucQLyAdapter = new DanhMucQLyAdapter(this,0,arrDanhMuc);
+            danhMucQLyAdapter = new DanhMucQLyAdapter(this,0,arrDanhMuc,this);
         }
         private void  anhXa(){
 
@@ -114,13 +122,14 @@
                     arrDanhMuc.add(new DanhMucQly(o));
                 }
                 //adapter.notifyDataSetChanged(); // Thông báo cho adapter biết dữ liệu đã thay đổi
-                danhMucQLyAdapter = new DanhMucQLyAdapter(this,0,arrDanhMuc);
+                danhMucQLyAdapter = new DanhMucQLyAdapter(this,0,arrDanhMuc,this);
                 lsvDanhMucQly.setAdapter(danhMucQLyAdapter);
 
             }catch (JSONException e){}
         }
 
         @Override
+
         public void biLoi() {
             Toast.makeText(this,"Loi Ket Noi",Toast.LENGTH_SHORT).show();
         }
@@ -148,5 +157,116 @@
                 return true;
             }
             return super.onOptionsItemSelected(item);
+        }
+
+        @Override
+        public void update(DanhMucQly danhMucQly) {
+            final Dialog dialog = new Dialog(QLyDanhMuc.this);
+            //We have added a title in the custom layout. So let's disable the default title.
+
+            //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+            dialog.setCancelable(true);
+            //Mention the name of the layout of your custom dialog.
+            dialog.setContentView(R.layout.dialog_update_danhmuc);
+            WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+            lp.copyFrom(dialog.getWindow().getAttributes());
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            lp.gravity = Gravity.CENTER;
+
+            dialog.getWindow().setAttributes(lp);
+
+            //Initializing the views of the dialog.
+            final EditText etname = dialog.findViewById(R.id.etName);
+            etname.setText(danhMucQly.getTenDanhMuc());
+            Button submitButton = dialog.findViewById(R.id.submit_button);
+            Button cancleButton = dialog.findViewById(R.id.cancle_button);
+
+
+
+
+            submitButton.setOnClickListener(v -> {
+                String nameCategory = etname.getText().toString();
+                if(nameCategory.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"Vui lòng không được bỏ trống thông tin.",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        "https://huf-android.000webhostapp.com/UpdateQLDM.php",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (response.trim().equals("success")) {
+                                    Toast.makeText(QLyDanhMuc.this, "Cập nhật thành công",Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                    new ApiDanhMucQLy(QLyDanhMuc.this).execute();
+                                } else {
+                                    Toast.makeText(QLyDanhMuc.this,"Cập nhật thất bại",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(QLyDanhMuc.this, "Xảy ra lỗi", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                ) {
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("CateID",danhMucQly.getId());
+                        params.put("newCateName",nameCategory);
+                        Log.d("nhan ", "getParams: "+nameCategory);
+                        return params;
+                    }
+                };
+                requestQueue.add(stringRequest);
+
+
+
+            });
+
+
+            cancleButton.setOnClickListener(v -> dialog.dismiss());
+
+            dialog.show();
+        }
+
+        @Override
+        public void delete(DanhMucQly danhMucQly) {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                    "https://huf-android.000webhostapp.com/DeleteQLDM.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Toast.makeText(QLyDanhMuc.this, response,Toast.LENGTH_SHORT).show();
+                            new ApiDanhMucQLy(QLyDanhMuc.this).execute();
+                        }
+                    },
+                    new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(QLyDanhMuc.this, "Xảy ra lỗi", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            ) {
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("CategoryID",danhMucQly.getId());
+                    Log.d("nhan ", "getParams: "+danhMucQly.getId());
+                    return params;
+                }
+            };
+            requestQueue.add(stringRequest);
         }
     }
---
+++ app/src/main/java/com/example/huflit/adapter/IOnclickItem.java
@@ -0,0 +1,9 @@
+package com.example.huflit.adapter;
+
+import com.example.huflit.object.DanhMucQly;
+
+public interface IOnclickItem {
+    void update(DanhMucQly danhMucQly);
+
+    void delete(DanhMucQly danhMucQly);
+}
